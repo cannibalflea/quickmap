d9070af5219d5b1668ed0565006fed0d4819edeb
Cannibal Flea <22717405+cannibalflea@users.noreply.github.com>
2021-05-10 00:44:13 -0700

    revised max zoom

--- script/quickmap.js
+++ script/quickmap.js
@@ -48,7 +48,7 @@ function setBasemap(basemapID) {
 
     basemapLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
+        maxZoom: 22,
         id: mapboxID,
         tileSize: 512,
         zoomOffset: -1,
