88953792d6e6ac7d0c58a150b4d12e0f381537df
Cannibal Flea <22717405+cannibalflea@users.noreply.github.com>
2021-05-10 18:13:34 -0700

    added property editor functionality

--- script/quickmap.js
+++ script/quickmap.js
@@ -10,7 +10,6 @@ var map = L.map('maparea', {
 
 });
 
-
 // get the url parameters
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -60,15 +59,12 @@ function setBasemap(basemapID) {
 if(urlParams.has('gj')) {
     // decompress the json object
     var objGeoJson = JSON.parse(JSONUncrush(decodeURIComponent(urlParams.get('gj'))));
-    console.log(objGeoJson);
 
     // load the geojson data onto the map
     var ftrGroup = L.geoJSON(objGeoJson, {
         style: function (feature) {
             //return {color: feature.properties.colour};
         }
-    }).bindPopup(function (layer) {
-        return layer.feature.properties.name;
     }).addTo(map);
 
     // if geometry was loaded, then fit map to bounds
@@ -116,11 +112,11 @@ map.pm.Toolbar.createCustomControl({
 // add custom control to choose basemap
 const bmapActions = [
     { 
-        text: '<i class="fas fa-map-marked-alt"></i>',
+        text: '<i class="fas fa-map-marked-alt leaflet-pm-cust-button"></i>',
         onClick: () => { setBasemap(1) }
     }, 
     {
-        text: '<i class="fas fa-satellite"></i>',
+        text: '<i class="fas fa-satellite leaflet-pm-cust-button"></i>',
         onClick: () => { setBasemap(2) }
     }
 ];
@@ -132,18 +128,106 @@ map.pm.Toolbar.createCustomControl({
     actions: bmapActions
 }); 
 
-// add custom control to add properties to the features
-const propActions = ['cancel'];
-map.pm.Toolbar.createCustomControl({   
-    name: 'Add property',  
-    block: 'custom', 
-    className: 'leaflet-pm-icon-prop',
-    title: 'Add property',
-    actions: propActions
-});
 
 // hide the map controls at startup
 map.pm.toggleControls();
 if(urlParams.has('gj')) {
     map.pm.toggleControls();
+}
+
+// setup the popup window views based on the data properties passed in
+map.pm.getGeomanLayers(true).eachLayer( function (layer){
+    var content = createPopupContent(layer,'view');
+    var test = layer.bindPopup(content);
+});
+
+// event listener for after layers are created
+map.on('pm:create', e => {  
+    var layer = e.layer;
+    var feature = layer.feature = layer.feature || {};
+
+    feature.type = feature.type || "Feature";
+    var props = feature.properties = feature.properties || {};
+    props.data = [];
+    
+    var content = createPopupContent(layer,'edit');
+    layer.bindPopup(content);
+});
+
+
+function createPopupContent(layer, type){
+    var props = layer.feature.properties;
+    switch(type){
+        case 'edit':
+            var fieldIndex = 0;
+            var content = '<div class="popup-title">FEATURE DATA  <i class="fas fa-save popup-title-button" onClick="saveData('+ layer._leaflet_id +')"></i></div>';
+            props.data.forEach(element => {
+                content += '<div class="popup-data-row" id="popup_data_row_'+ fieldIndex +'"><input type="text" id="data_name_' + fieldIndex + '" value="' + element[0] + '" onchange="updateData('+ fieldIndex +', '+ layer._leaflet_id +')"> <input type="text" id="data_value_' + fieldIndex + '" value="' + element[1] + '" onchange="updateData('+ fieldIndex +', '+ layer._leaflet_id +')"> <i class="fas fa-minus-square popup-row-button" style="color: red;" onClick="removeData('+ fieldIndex +', '+ layer._leaflet_id +')"></i></div>';
+                fieldIndex++;
+            });
+            content += '<div class="popup-data-row" id="popup_data_add"><input type="text" id="data_add_name"> <input type="text" id="data_add_value">  <i class="fas fa-plus-square popup-row-button" style="color: green;" onClick="addData('+ layer._leaflet_id +')"></i></div>';
+            break;
+        case 'view':
+            var content = '<div class="popup-title">FEATURE DATA <i class="fas fa-edit popup-title-button" onClick="editData('+ layer._leaflet_id +')"></i></div><div class="popup-data-table">';
+            var fieldIndex = 1;
+            props.data.forEach(element => {
+                content += '<div class="popup-data-row-view'+ ((fieldIndex % 2 === 0) ? ' popup-data-row-highlight': '') +'"><div class="popup-data-cell"><strong>'+ element[0] + '</strong></div> <div class="popup-data-cell" style="width:100%;">' + element[1] + '</div></div>';
+                fieldIndex++;
+            });
+            content += "</div>";
+    }
+    return content;
+}
+
+function editData(layerID){
+    // set the popup into edit mode
+    var layer = map.pm.getGeomanLayers(true).getLayer(layerID);
+    var content = createPopupContent(layer,'edit');
+    layer.setPopupContent(content);
+}
+
+function saveData(layerID){
+    // set the popup into view mode
+    var layer = map.pm.getGeomanLayers(true).getLayer(layerID);
+    var content = createPopupContent(layer,'view');
+    layer.setPopupContent(content);
+}
+
+function addData(layerID) {
+    // get data to be added
+    var newName = document.querySelector('#data_add_name').value;
+    var newValue = document.querySelector('#data_add_value').value;
+
+    // add the new key/value pair to the layer data
+    var layer = map.pm.getGeomanLayers(true).getLayer(layerID);
+    layer.feature.properties.data.push([newName , newValue]);
+
+    // reset the popup content
+    var content = createPopupContent(layer,'edit');
+    layer.setPopupContent(content);
+}
+
+function removeData(index, layerID) {
+    // remove the data row from the map element
+    var layer = map.pm.getGeomanLayers(true).getLayer(layerID);
+    layer.feature.properties.data.splice(index, 1);
+
+    // reset the popup content
+    var content = createPopupContent(layer,'edit');
+    layer.setPopupContent(content);
+}
+
+function updateData(index, layerID){
+    // get updated values
+    var newName = document.querySelector('#data_name_'+index).value;
+    var newValue = document.querySelector('#data_value_'+index).value;
+    var updatedData = [newName, newValue];
+    
+    // remove the data row from the map element
+    var layer = map.pm.getGeomanLayers(true).getLayer(layerID);
+    layer.feature.properties.data.splice(index, 1, updatedData);
+
+    // reset the popup content
+    var content = createPopupContent(layer,'edit');
+    layer.setPopupContent(content);
 }
